1607bfc74538b549c0c1ecb20f0d594273aa1558
PolinaFaustova <polinamusikerin@gmail.com>
2023-04-29 23:23:39 +0200

    first task ready/css ok

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,31 +1,32 @@
 import PropTypes from 'prop-types';
+import css from './Profile.module.css'
 
 export const Profile = ({username, tag, location, avatar, followers, views, likes }) => {
     return (
-        <div className="profile">
-      <div className="description">
+        <div className={css.profile}>
+      <div className={css.description}>
         <img
           src={avatar}
           alt={username + 'avatar'}
-          className="avatar"
+          className={css.avatar}
         />
-        <p className="name">{username}</p>
-        <p className="tag">@{tag}</p>
-        <p className="location">{location}</p>
+        <p className={css.name}>{username}</p>
+        <p className={css.tag}>@{tag}</p>
+        <p className={css.location}>{location}</p>
       </div>
     
-      <ul className="stats">
+      <ul className={css.stats}>
         <li>
-          <span className="label">Followers</span>
-          <span className="quantity">{followers}</span>
+          <span className={css.label}>Followers</span>
+          <span className={css.quantity}>{followers}</span>
         </li>
         <li>
-          <span className="label">Views</span>
-          <span className="quantity">{views}</span>
+          <span className={css.label}>Views</span>
+          <span className={css.quantity}>{views}</span>
         </li>
         <li>
-          <span className="label">Likes</span>
-          <span className="quantity">{likes}</span>
+          <span className={css.label}>Likes</span>
+          <span className={css.quantity}>{likes}</span>
         </li>
       </ul>
     </div>
