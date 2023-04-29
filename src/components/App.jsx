90085627f147eb3509a15f4875022b243dcca3a5
PolinaFaustova <polinamusikerin@gmail.com>
2023-04-29 16:31:02 +0200

    start working on

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,17 +2,8 @@
 // внутри общего контейнера - корневого компонента <App>.
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+    {/* <Profile /> */}
     </div>
   );
 };
