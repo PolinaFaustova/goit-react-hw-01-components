508074d98d3ecf6362625cf022b28255dd866469
PolinaFaustova <polinamusikerin@gmail.com>
2023-05-02 10:51:35 +0200

    Update TransactionHistory.jsx

--- src/components/TransactionHistory/TransactionHistory.jsx
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -27,7 +27,7 @@ export const TransactionHistory = ({ items }) => {
     )
 };
     TransactionHistory.propTypes = {
-    items: PropTypes.arrayOf(PropTypes.shape({
+        items: PropTypes.arrayOf(PropTypes.shape({
         id: PropTypes.string.isRequired,
         type: PropTypes.string.isRequired,
         amount: PropTypes.string.isRequired,
